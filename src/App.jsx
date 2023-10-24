4d12631f74a221743b2a29d90f7731e105033bb0
Blessing U <blessing986@users.noreply.github.com>
2023-10-24 22:40:36 +0100

    chore: collecting user input with refs and fowarded refs

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,33 @@ function App() {
   });
 
   function handleStartAddProject() {
-    setProjectsState((prevStae) => {
+    setProjectsState((prevState) => {
       return {
-        ...prevStae,
+        ...prevState,
         selectedProjectId: null,
       };
     });
   }
 
+  const handleAddProject = (projectData) => {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
