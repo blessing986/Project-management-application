2de7d0677fa4ee43e1ac73d5a9109d56d1282971
Blessing U <blessing986@users.noreply.github.com>
2023-10-26 10:27:21 +0100

    chore: handling project creation and updating the UI

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,18 @@ function App() {
 
   const handleAddProject = (projectData) => {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   };
-
-  console.log(projectsState);
 
   let content;
 
@@ -43,7 +43,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
